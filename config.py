2334db2ef8c75404d3eb0a290c97b87066fe07b4
Jaedonlkh <103723147+Jaedonlkh@users.noreply.github.com>
2022-04-21 21:16:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.czvrowtwhg9s.us-east-1.rds.amazonaws.com" /your endppoint
+customuser = "aws_user"
+custompass = "CAPC3009"
+customdb = "employee"
+custombucket = "jaedon-employee"
+customregion = "us-east-1"
